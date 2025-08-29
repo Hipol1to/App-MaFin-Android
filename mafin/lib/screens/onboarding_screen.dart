566af20fd9f7d55067a60f5798e8cd0c8ef4db84
Hipol1to <hipolitoprz2001@gmail.com>
2025-08-28 23:02:24 -0400

    splash screen started

---
+++ mafin/lib/screens/onboarding_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Here we will put the onboarding screen.'),
+        ),
+      ),
+    );
+  }
+}
